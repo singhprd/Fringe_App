2a65c4f39a247e3cb6388c75a74c5857d01d0ce1
Peter Singh <peter@Peters-iMac.home>
2016-09-07 21:38:23 +0100

    reverted and scaffolded Votes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :votes
   devise_for :users
   resources :reviews
   resources :searches
   resources :performances
   resources :events
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   post 'search' => 'search#do_search'
