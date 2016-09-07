f018226d1548c04364bfcf23e237c7e3c202de8b
Peter Singh <peter@Peters-iMac.home>
2016-09-07 21:31:59 +0100

    generated votes model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907193532) do
+ActiveRecord::Schema.define(version: 20160907203043) do
 
   create_table "events", force: :cascade do |t|
     t.string   "age_category"
@@ -82,6 +82,16 @@ ActiveRecord::Schema.define(version: 20160907193532) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.binary   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_votes_on_event_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
 end
