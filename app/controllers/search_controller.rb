e4afd4f9d9f376f133968dcf26ec0a5b66ec7f06
Peter Singh <peter@Peters-MBP.home>
2016-11-16 19:14:21 +0000

    improved search params

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,8 @@ class SearchController < ApplicationController
 	protect_from_forgery with: :null_session
 
 	def do_search
-		title_string = params[:title]
 		fb = Fringebot.new()
-		@results = fb.search(title_string)
+		@results = fb.search(params)
 		@results
 	end
 
