faaf84360a26c5d64d6046451f5d575188790d29
Peter Singh <singh.prd@gmail.com>
2017-04-08 19:20:46 +0100

    ordered favourites tab by score

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
   def welcome
     @events = Event.all.order(score: :desc).page(params[:page]).per(5)
     if params[:favourites]
-      @events_raw = Event.find(current_user.favourites.map(&:event_id))
+      @events_raw = Event.order(score: :desc).find( current_user.favourites.map(&:event_id) )
       @events = Kaminari.paginate_array(@events_raw).page(params[:page]).per(5)
     end
   end
