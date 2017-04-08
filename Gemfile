61e45d04da5ceb694ee9eec14c542c3b99b63594
Peter Singh <singh.prd@gmail.com>
2017-04-08 17:40:46 +0100

    fixed bootstrap issues

--- Gemfile
+++ Gemfile
@@ -79,3 +79,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'festivals_lab'
+
+gem "font-awesome-rails"
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,6 @@ module Fringe
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+	config.assets.version = '1.0'
   end
 end
