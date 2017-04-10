4de70043002932396b9a4dbbfecfea922991c694
Peter Singh <singh.prd@gmail.com>
2017-04-11 00:55:29 +0100

    user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	setup do
+    @event = events(:event_one)
+    @new_event = events(:new_event)
+    @user_two = users(:user_two)
+    @admin_user_one = users(:admin_user_one)
+  end
+  
+  test "test_has_10_votes_by_default" do
+    assert_equal(10, @user_two.votes_left)
+  end
+  
+  test "reduce_votes_left" do
+  	assert @user_two.reduce_votes_left
+    assert_equal(9, @user_two.votes_left)
+  end
+
+  test "votes_dont_go_negative" do
+  	@user_two.votes_left = 2
+  	@user_two.save
+  	assert @user_two.reduce_votes_left
+    assert_equal(1, @user_two.votes_left)
+  	assert @user_two.reduce_votes_left
+  	assert_equal false, @user_two.reduce_votes_left
+  	assert_equal 0, @user_two.votes_left
+  end
+
 end
