d99b717d765b1452a43ca237742daed3a93c6445
Peter Singh <singh.prd@gmail.com>
2017-04-11 00:36:55 +0100

    tests

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -4,10 +4,28 @@ class EventTest < ActiveSupport::TestCase
   setup do
     @event = events(:event_one)
     @user_two = users(:user_two)
+    @vote1 = votes(:vote_1)
+  	@vote2 = Vote.new({user_id: @user_two.id, event_id: @event.id, value: 1 }).save
+  	@vote3 = Vote.new({user_id: @user_two.id, event_id: @event.id, value: 1 }).save
+  	@vote4 = Vote.new({user_id: @user_two.id, event_id: @event.id, value: -1 }).save
+  	@vote5 = Vote.new({user_id: @user_two.id, event_id: @event.id, value: -1 }).save
   end
   
   test "favourited?" do
     assert_equal( false, Event.favourited?(@user_two, @event.id))
   end
+
+  test "test_upvotes" do
+		assert_equal(2, @event.upvotes)
+  end
+
+  test "test_downvotes" do
+  	assert_equal(2, @event.downvotes)
+  end
+
+  test "test_tally_votes" do
+  	assert_equal(0, @event.tally_votes)
+  end
+
 
 end
--- test/models/favourite_test.rb
+++ test/models/favourite_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class FavouriteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	
+	setup do
+		@user_two = users(:user_two)
+    @event = events(:event_one)
+  	@vote = Vote.create({user_id: @user_two.id, event_id: @event.id, value: 1 }).save
+  	@favourite = Favourite.create({event: @event, user: @user_two})
+	end
+
+  test "easy_find_both_params" do
+    assert_equal(@favourite, Favourite.easy_find({event_id: @event.id, user_id: @user_two.id}))
+  end
+
+  test "easy_find_user" do
+    assert_equal(@favourite, Favourite.easy_find({user_id: @user_two.id}).first)
+  end
+
+  test "easy_find_returns_collection" do
+    assert(Favourite.easy_find({user_id: @user_two.id}).is_a?(Favourite::ActiveRecord_Associations_CollectionProxy))
+  end
+
+  test "easy_find_event" do
+    assert_equal(@favourite, Favourite.easy_find({event_id: @event.id}).first)
+    assert_equal(@favourite, Favourite.easy_find({event_id: @event.id}).first)
+  end
+
+
 end
