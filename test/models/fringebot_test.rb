00b79ceb3518021c0876aa27554108eb4435f8c1
Peter Singh <singh.prd@gmail.com>
2017-04-19 22:24:13 +0100

    fixed search

--- test/models/fringebot_test.rb
+++ test/models/fringebot_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 # TODO Give example hashes for each type
 class FringebotTest < ActiveSupport::TestCase
 	setup do
-		@fringebot = Fringebot.new({title: "abc"})
+		@fringebot = Fringebot.new({title_string: "FAKE"})
+		@fringebot_book = Fringebot.new({festival_string: "book"})
 		@api_response = @fringebot.query_api
 		@results = @fringebot.get_events
 	end
@@ -27,7 +28,10 @@ class FringebotTest < ActiveSupport::TestCase
 		assert_equal Array, @fringebot.create_performances_params_array(@api_response.first).class
 		assert_equal [Hash], @fringebot.create_performances_params_array(@api_response.first).map(&:class).uniq
 	end
-
-
+	
+	test "filtering to book festival returns only book festival results" do
+		book_events = @fringebot_book.get_events
+		assert_equal false, book_events
+	end
 
 end
