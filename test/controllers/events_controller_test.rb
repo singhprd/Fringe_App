90a0c8076e46e982bf7b8bd42c3d44c0bd004edf
Peter Singh <peter@Peters-iMac.home>
2017-04-23 23:28:22 +0100

    removed controller tests for the moment

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -10,54 +10,54 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     sign_in @admin_user
   end
 
-  test "should get is_favourited?" do
-    get events_url
-    assert_response :success
-  end
-
-  test "should get index" do
-    get events_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_event_url
-    assert_response :success
-  end
-
-  # TODO: Need to test against the API data!
-  # test "should create event" do
-  #   # puts "somethting"
-  #   # params = Fabricate.attributes_for(:event)
-  #   # puts params
-  #   # raise
-  #   post events_url, params: Fabricate.attributes_for(:event)
-  #   assert_equal true, Fabricate.attributes_for(:event)
+  # test "should get is_favourited?" do
+  #   get events_url
+  #   assert_response :success
   # end
 
-  #   assert_redirected_to event_url(Event.last)
+  # test "should get index" do
+  #   get events_url
+  #   assert_response :success
   # end
 
-  test "should show event" do
-    get event_url(@event)
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get new_event_url
+  #   assert_response :success
+  # end
 
-  # TODO Test Once Admin User Setup and Signed in
-  test "should get edit" do
-    get edit_event_url(@event)
-    assert_response :success
-  end
+  # # TODO: Need to test against the API data!
+  # # test "should create event" do
+  # #   # puts "somethting"
+  # #   # params = Fabricate.attributes_for(:event)
+  # #   # puts params
+  # #   # raise
+  # #   post events_url, params: Fabricate.attributes_for(:event)
+  # #   assert_equal true, Fabricate.attributes_for(:event)
+  # # end
 
-  test "should update event" do
-    patch event_url(@event), params: Fabricate.attributes_for(:event)
-    assert_redirected_to event_url(@event)
-  end
+  # #   assert_redirected_to event_url(Event.last)
+  # # end
 
-  test "should destroy event" do
-    assert_difference("Event.count", -1) do
-      delete event_url(@event)
-    end
-    assert_redirected_to events_url
-  end
+  # test "should show event" do
+  #   get event_url(@event)
+  #   assert_response :success
+  # end
+
+  # # TODO Test Once Admin User Setup and Signed in
+  # test "should get edit" do
+  #   get edit_event_url(@event)
+  #   assert_response :success
+  # end
+
+  # test "should update event" do
+  #   patch event_url(@event), params: Fabricate.attributes_for(:event)
+  #   assert_redirected_to event_url(@event)
+  # end
+
+  # test "should destroy event" do
+  #   assert_difference("Event.count", -1) do
+  #     delete event_url(@event)
+  #   end
+  #   assert_redirected_to events_url
+  # end
 end
