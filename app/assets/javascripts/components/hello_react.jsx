9bfbf3025e3c778ac18e148c2ac8aecce253b0cc
Peter Singh <singh.prd@gmail.com>
2017-05-21 13:32:28 +0100

    yarn and webpack setup - but no props can be passed

---
+++ app/assets/javascripts/components/hello_react.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const HelloReact = React.createClass({
+	render() {
+		return (
+			<div>
+				<h1>BATMANASDADASD</h1>
+			</div>
+		);
+	}
+});
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,5 +8,3 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker')
-
-var reactCountdownClock = require("react-countdown-clock")
