d48be59ea480e5f9740dd000706e554edf2d682b
Peter Singh <singh.prd@gmail.com>
2017-07-12 23:12:03 +0100

    set default votes to 100

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  after_initialize :set_default_votes
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -17,5 +19,9 @@ class User < ApplicationRecord
   def reset_votes
     vote_reset_amount = self.vote_reset_amount
     update_attributes(votes_left: vote_reset_amount)
+  end
+
+  def set_default_votes
+    update(votes_left: 100)
   end
 end
