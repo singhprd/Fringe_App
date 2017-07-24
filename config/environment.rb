b449f7c749e733c70c47e88e0c00e5c88976daae
Peter Singh <singh.prd@gmail.com>
2017-07-24 23:16:21 +0100

    5.1 upgrade

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActiveSupport::Deprecation.silenced = true
+ActiveSupport::Deprecation.silenced = false
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   get 'hello_world', to: 'hello_world#index'
   resources :favourites
   resources :venues
