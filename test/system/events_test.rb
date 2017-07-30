76a050e9cc8651ae5619c521032b0e1ebd188844
Peter Singh <singh.prd@gmail.com>
2017-07-30 02:42:42 +0100

    better system tests

--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -2,17 +2,69 @@ require "application_system_test_case"
 
 class EventsTest < ApplicationSystemTestCase
   test "visiting the search form and verify results" do
-    visit "/search"
+    visit "/"
+    click_on('Sign in')
+    
+    click_on('Sign up')
+    fill_in('Email', :with => 'test@test.com')
+    fill_in('Username', :with => 'test username')
+    
+    fill_in('Password', :with => 'password')
+    fill_in('Password confirmation', :with => 'password')
+    
+    click_button("Sign up")
+    
+    click_on("Account")
+    click_on("Sign out")
+
+    click_on("Sign in")
+    fill_in('Email', :with => 'test@test.com')
+    fill_in('Password', :with => 'password')
+
+    click_button("Log in")
+
+    has_current_path?('/')
+
+    visit "/searches/new"
     assert_selector "#search-panel", text: "Search"
     select("2016", :from => "Year")
     select("Edinburgh's Hogmanay", :from => "Festival")
     click_button('Search')
     
     page.has_selector?('home_card', count: 5)
-    click_link("Concert in the Gardens")
+    click_on("Concert in the Gardens")
+    
+    click_on("❤️️")
     
     page.has_selector?('home_card', count: 1)
     assert_selector "a", text: "Concert in the Gardens"
+
+    click_on("🗓️")
+    page.has_table?('performances_for_event24')
+    
+    page.assert_selector('td', :text => 'Saturday 31st December 2016', :visible => true)
+    page.assert_selector('td', :text => '20:00', :visible => true)
+    page.assert_selector('td', :text => '01:00', :visible => true)
+    
+    click_on("🗓️")
+    page.has_no_table?('performances_for_event24')
+
+    page.assert_selector('button', :text => '0', :visible => true)
+    click_on("👍")
+    click_on("👍")
+    click_on("👍")
+    page.assert_selector('button', :text => '3', :visible => true)
+
+
+    # click_on("Home")
+    # click_button("Favourite")
+    
+    # click_button("2016")
+    
+    # navigate().refresh();
+
+    # page.has_selector?('home_card', count: 1)
+    # assert_selector "a", text: "Concert in the Gardens"
 
     # click_link("🗓️")
     # assert_selector "#performances_for_event183"
---
+++ test/system/sesssions_test.rb
@@ -0,0 +1,41 @@
+require "application_system_test_case"
+
+class SessionsTest < ApplicationSystemTestCase
+  # test "login" do
+  #   visit "/"
+  #   click_on('Sign in')
+    
+  #   click_on('Sign up')
+  #   fill_in('Email', :with => 'test@test.com')
+  #   fill_in('Username', :with => 'test username')
+    
+  #   fill_in('Password', :with => 'password')
+  #   fill_in('Password confirmation', :with => 'password')
+    
+  #   click_button("Sign up")
+    
+  #   click_on("Account")
+  #   click_on("Sign out")
+
+  #   click_on("Sign in")
+  #   fill_in('Email', :with => 'test@test.com')
+  #   fill_in('Password', :with => 'password')
+
+  #   click_button("Log in")
+
+  #   has_current_path?('/')
+
+    # assert_selector "#search-panel", text: "Search"
+    # select("2016", :from => "Year")
+    # select("Edinburgh's Hogmanay", :from => "Festival")
+    
+    # page.has_selector?('home_card', count: 5)
+    # click_link("Concert in the Gardens")
+    
+    # page.has_selector?('home_card', count: 1)
+    # assert_selector "a", text: "Concert in the Gardens"
+
+    # click_link("🗓️")
+    # assert_selector "#performances_for_event183"
+  # end
+end
