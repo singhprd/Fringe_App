73d96c14f4596387291b978e6d8feda5d5fa6bc9
Peter Singh <peter@peters-mbp.home>
2017-11-11 17:55:09 +0000

    fix

--- config/initializers/react_on_rails.rb
+++ config/initializers/react_on_rails.rb
@@ -23,7 +23,6 @@ ReactOnRails.configure do |config|
   # This configures the script to run to build the production assets by webpack. Set this to nil
   # if you don't want react_on_rails building this file for you.
   config.npm_build_production_command = "yarn run build:production"
-  config.build_production_command = "yarn run build:production"
 
   ################################################################################
   # CLIENT RENDERING OPTIONS
