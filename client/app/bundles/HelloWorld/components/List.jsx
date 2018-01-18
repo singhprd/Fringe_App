b216e25031679cd9371d644d7360d06fe4981ee6
Peter Singh <peter@peters-mbp.home>
2018-01-18 20:14:50 +0000

    list formatting

--- client/app/bundles/HelloWorld/components/List.jsx
+++ client/app/bundles/HelloWorld/components/List.jsx
@@ -1,19 +1,26 @@
-import React, {Component} from 'react';
-import {render} from 'react-dom';
-import EventCard from './EventCard.jsx';
-import ListItem from './ListItem.jsx';
-import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import EventCard from "./EventCard.jsx";
+import ListItem from "./ListItem.jsx";
+import {
+  SortableContainer,
+  SortableElement,
+  arrayMove
+} from "react-sortable-hoc";
+import PropTypes from "prop-types";
 
-const SortableItem = SortableElement(({value}) =>
-  <div>{value}</div>
-);
+const SortableItem = SortableElement(({ value }) => <div>{value}</div>);
 
-const SortableList = SortableContainer(({items}) => {
+const SortableList = SortableContainer(({ items }) => {
   return (
     <div>
       {items.map((value, index) => (
-        <SortableItem key={`item-${index}`} sortIndex={value.props.positon} index={index} value={value} />
+        <SortableItem
+          key={`item-${index}`}
+          sortIndex={value.props.positon}
+          index={index}
+          value={value}
+        />
       ))}
     </div>
   );
@@ -31,40 +38,57 @@ class List extends Component {
     listId: PropTypes.number
   };
   state = {
-    items: this.listEventCardItems(),
+    items: this.listEventCardItems()
   };
-  getEvent(id){
-    var events = this.props.events.filter(function( event ) {
+  getEvent(id) {
+    var events = this.props.events.filter(function(event) {
       var event = JSON.parse(event);
       return event.id === id;
     });
     return JSON.parse(events[0]);
   }
-  listEventCardItems(){
-    var items = this.props.listItems.map(function(item, index, arr){
-      var eventJson = this.getEvent(item.event_id);
-      console.log(typeof(eventJson))
-      return <EventCard userSignedIn key={item.event_id} eventId={item.event_id} short={true} positon={item.position} eventJson={eventJson} />;
-    }.bind(this));
-    return items.sort(function(a, b){return a.props.positon - b.props.positon;});
-  }
-  swapPositon(oldIndex, newIndex, listId){
-    $.ajax({
-      type: 'POST',
-      url: '/lists/swap_items',
-      data: {oldIndex: oldIndex, newIndex: newIndex, listId: listId}
+  listEventCardItems() {
+    var items = this.props.listItems.map(
+      function(item, index, arr) {
+        var eventJson = this.getEvent(item.event_id);
+
+        return (
+          <EventCard
+            userSignedIn
+            key={item.event_id}
+            eventId={item.event_id}
+            short={true}
+            positon={item.position}
+            eventJson={eventJson}
+          />
+        );
+      }.bind(this)
+    );
+    return items.sort(function(a, b) {
+      return a.props.positon - b.props.positon;
     });
   }
-  onSortEnd = ({oldIndex, newIndex, collection}) => {
+  swapPositon(oldIndex, newIndex, listId) {
+    $.ajax({
+      type: "POST",
+      url: "/lists/swap_items",
+      data: { oldIndex: oldIndex, newIndex: newIndex, listId: listId }
+    });
+  }
+  onSortEnd = ({ oldIndex, newIndex, collection }) => {
     this.swapPositon(oldIndex, newIndex, this.props.listId);
     this.setState({
-      items: arrayMove(this.state.items, oldIndex, newIndex),
+      items: arrayMove(this.state.items, oldIndex, newIndex)
     });
   };
   render() {
-    return(
-      <div>
-        <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-8">
+            <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />
+          </div>
+        </div>
       </div>
     );
   }
