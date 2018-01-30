cedcddf69208ad17cc991b0b15548f9fb94bfadc
Peter Singh <peter@peters-mbp.home>
2018-01-30 20:55:45 +0000

    allow delete lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,5 @@
 class List < ApplicationRecord
-  has_many :list_items, -> { order(position: :asc) }
+  has_many :list_items, -> { order(position: :asc) }, :dependent => :destroy
   has_many :events, :through => :list_items
   belongs_to :user
 end
