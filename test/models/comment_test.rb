53202d6e7c24bcf2bf91201f74db9626ac357822
Peter Singh <singh.prd@gmail.com>
2018-02-18 20:22:10 +0000

    Add spinner to search button when searching

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+  test "the truth 2" do
+    assert true
+  end
+  test "the truth 4" do
+    assert true
+  end
+  test "the truth 10" do
+    assert true
+  end
+
 end
