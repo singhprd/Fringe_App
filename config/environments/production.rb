30ba24492f4e638e0d1345caeb9cfd0c67bd3e74
Peter Singh <singh.prd@gmail.com>
2019-02-07 20:25:20 +0000

    add mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,15 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "fringe_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :mailgun
+
+  config.action_mailer.mailgun_settings = {
+    api_key: ENV["FRINGE_MAILGUN_API_KEY"],
+    domain: 'mg.edinfringe.co.uk',
+  }
+
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
