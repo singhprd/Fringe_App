dc3a26a3928c35790b164d1daf83cf89306ac20a
Peter Singh <singh.prd@gmail.com>
2019-02-09 21:30:52 +0000

    create credentials

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190209211955) do
+ActiveRecord::Schema.define(version: 20190209212815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 20190209211955) do
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_comments_on_event_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "credentials", force: :cascade do |t|
+    t.string "external_id"
+    t.string "public_key"
+    t.bigint "user_id"
+    t.string "nickname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_credentials_on_user_id"
   end
 
   create_table "events", id: :serial, force: :cascade do |t|
@@ -230,6 +240,7 @@ ActiveRecord::Schema.define(version: 20190209211955) do
   add_foreign_key "comment_votes", "users"
   add_foreign_key "comments", "events"
   add_foreign_key "comments", "users"
+  add_foreign_key "credentials", "users"
   add_foreign_key "events", "venues"
   add_foreign_key "favourites", "events"
   add_foreign_key "favourites", "users"
