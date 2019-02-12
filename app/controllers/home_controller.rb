43979c9530ad3edbb3c5000cd1081ab08c105a1d
Peter Singh <singh.prd@gmail.com>
2019-02-12 01:08:18 +0000

    added search bar in right place

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
 
   # GET /
   def welcome
+    # binding.pry
     params[:favourites] ||= 'false'
     params[:year] ||= Fringebot::YEARS.last
 
--- client/app/bundles/React/components/FilterBar.jsx
+++ client/app/bundles/React/components/FilterBar.jsx
@@ -38,21 +38,53 @@ export class FilterBar extends Component {
         return 'active';
       }
     }
+    search() {
+      var params = this.params();
+      // Update the params to change the filter
+      // params[param] = value;
+      params["searchTerm"] = document.getElementById("searchInput").value()
+      Turbolinks.clearCache();
+      Turbolinks.visit('/?' + $.param(params), {'action':'replace'});      
+    }
     render() {
+      var divStyle = {
+        height: '30px',
+      };
+      var group1style = {
+        marginRight:'5px',
+      }
       return (
         <div className="btn-toolbar">
-          <div className="btn-group">
-            <button onClick={() => this.redirect('favourites', 'false')} className={'btn btn-primary navbar-btn btn-sm ' + (this.isActive('favourites', 'false'))}>All
-            </button>
-            <button onClick={() => this.redirect('favourites', 'true')} className={'btn btn-primary navbar-btn btn-sm ' + (this.isActive('favourites', 'true'))}>Favourites</button>
-          </div>
-          <div className="btn-group">
-            <button onClick={() => this.redirect('year', '2015')} className={'btn btn-primary navbar-btn btn-sm ' + (this.isActive('year', '2015'))}>2015</button>
-            <button onClick={() => this.redirect('year', '2016')} className={'btn btn-primary navbar-btn btn-sm ' + (this.isActive('year', '2016'))}>2016</button>
-            <button onClick={() => this.redirect('year', '2017')} className={'btn btn-primary navbar-btn btn-sm ' + (this.isActive('year', '2017'))}>2017</button>
-            <button onClick={() => this.redirect('year', '2018')} className={'btn btn-primary navbar-btn btn-sm ' + (this.isActive('year', '2018'))}>2018</button>
-          </div>
-        </div>
+          <div className="row">
+            <div className="col-sm-6">
+              <div className="btn-group" role="group" style={group1style}>
+                <button onClick={() => this.redirect('favourites', 'false')} className={'btn btn-primary navbar-btn btn-sm ' + (this.isActive('favourites', 'false'))}>All</button>
+                <button onClick={() => this.redirect('favourites', 'true')} className={'btn btn-primary navbar-btn btn-sm ' + (this.isActive('favourites', 'true'))}>Favourites</button>
+              </div>
+              <div className="btn-group">
+                <button onClick={() => this.redirect('year', '2015')} className={'btn btn-primary navbar-btn btn-sm ' + (this.isActive('year', '2015'))}>2015</button>
+                <button onClick={() => this.redirect('year', '2016')} className={'btn btn-primary navbar-btn btn-sm ' + (this.isActive('year', '2016'))}>2016</button>
+                <button onClick={() => this.redirect('year', '2017')} className={'btn btn-primary navbar-btn btn-sm ' + (this.isActive('year', '2017'))}>2017</button>
+                <button onClick={() => this.redirect('year', '2018')} className={'btn btn-primary navbar-btn btn-sm ' + (this.isActive('year', '2018'))}>2018</button>
+              </div>
+            </div>
+
+            <div className="col-sm-6">
+              <div className="btn-group">
+              <form>
+                <div className="input-group">
+                  <input style={divStyle} type="text" className="form-control btn btn-default navbar-btn btn-sm"  placeholder="Search"/>
+                  <div className="input-group-btn">
+                    <button id="searchInput" onClick={() => this.search()} className="btn btn-default navbar-btn btn-sm" type="submit" style={divStyle}>
+                      <i className="glyphicon glyphicon-search"></i>
+                    </button>
+                  </div>
+                </div>
+              </form>
+              </div>
+            </div>
+          </div>    
+        </div>    
       );
     }
 }
