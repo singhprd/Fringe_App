6dad1588ee46a66ee110ec16d38cedcb8f81a1d6
Peter Singh <singh.prd@gmail.com>
2019-07-13 00:36:39 +0100

    things

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,14 +1,37 @@
 # frozen_string_literal: true
 
 module ListsHelper
+  def friend_action(user)
+    # if !current_user.friends_with?(user)
+    #   return content_tag(:span, "Friend", {
+    #     data: {
+    #       action: "click->friend-list#friendUser",
+    #       user_id: user.id,
+    #     },
+    #     style: "cursor: pointer;",
+    #     class: "pull-right",
+    #   })
+    # end
+  end
+
+  def separator
+    return content_tag(:span, "&nbsp;|&nbsp;".html_safe, {class: "pull-right"})
+  end
+
   def user_list_action(user)
-    content_tag(:a, "Remove!", {
-      data: {
-        action: "click->friend-list#removeUser",
-        user_id: user.id,
-      },
-      style: "cursor: pointer;",
-      class: "pull-right",
-    })
+    if user == current_user
+      return
+    end
+
+    if current_user == @list.owner
+      return content_tag(:span, "Remove", {
+        data: {
+          action: "click->friend-list#removeUser",
+          user_id: user.id,
+        },
+        style: "cursor: pointer;",
+        class: "pull-right",
+      })
+    end
   end
 end
